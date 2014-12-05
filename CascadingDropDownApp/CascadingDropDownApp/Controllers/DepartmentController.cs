ba4e49b2c21505bf13abfb98092e623bc2b3c074
nrifath2009 <nrifath2009@gmail.com>
2014-12-06 03:28:06 +0600

    Cascading Dropdown Example in MVC

---
+++ CascadingDropDownApp/CascadingDropDownApp/Controllers/DepartmentController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CascadingDropDownApp.Models;
+
+namespace CascadingDropDownApp.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private List<Student> students = new List<Student>()
+        {
+            
+            new Student(){StudentId = 1,Name = "A",DepartmentId = 1},
+             new Student(){StudentId = 1,Name = "B",DepartmentId = 2},
+             new Student(){StudentId = 1,Name = "D",DepartmentId = 3},
+             new Student(){StudentId = 1,Name = "E",DepartmentId = 3},
+             new Student(){StudentId = 1,Name = "G",DepartmentId = 1},
+             new Student(){StudentId = 1,Name = "J",DepartmentId = 2}
+
+
+        }; 
+        //
+        // GET: /Department/
+        public ActionResult Index()
+        {
+            List<Department> departments = new List<Department>
+            {
+                new Department()
+                {
+                    DepartmentId = 1, Name = "Computer Science"
+                },
+                new Department()
+                {
+                    DepartmentId = 2, Name = "Electrical & Electronics"
+                },
+                 new Department()
+                {
+                    DepartmentId = 3, Name = "Information Technology"
+                }
+
+            };
+
+            ViewBag.Departments = departments;
+            return View();
+        }
+
+        public JsonResult GetStudentsByDepartmentId(int departmentId)
+        {
+            var studentList = students.Where(a => a.DepartmentId == departmentId).ToList();
+            return Json(studentList, JsonRequestBehavior.AllowGet);
+
+        }
+        //[HttpPost]
+        //public List<Student> GetStudentsByDepartmentId(int departmentId)
+        //{
+        //    List<Student> studentList = students.Where(a => a.DepartmentId == departmentId).ToList();
+        //    return studentList;
+
+        //}
+
+	}
+}
